71d01270bd586ba334b9bf84a61f06fd4e3285ab
Nolan Schock <schocknol@gmail.com>
2024-05-09 08:08:21 -0500

    Some geometric verification implementation details

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
+#include <Eigen/Core>
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <opencv2/features2d.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d.hpp>
+#include <random>
 
+#include "feature_matching.h"
+#include "geometric_verification.h"
 #include "image_utils.h"
 
 int main() {
   // Load images
-  std::string directory{readDirectory()};
-  std::vector<std::string> extensions{parseExtensions(readExtensions())};
+  // std::string directory{readDirectory()};
+  std::string directory{"../images"};
+  // std::vector<std::string> extensions{parseExtensions(readExtensions())};
+  std::vector<std::string> extensions = {".png"};
   auto [images, names] = loadImages(directory, extensions, cv::IMREAD_GRAYSCALE);
 
   // Compute features
@@ -24,19 +30,35 @@ int main() {
   // For now we're just matching features from the first two images for testing purposes. Will later adapt to match
   // across entire list of images.
   matcher->match(descriptors[0], descriptors[1], matches);
+  std::vector<MatchPoints> matchPoints{getMatchPoints(keypoints[0], keypoints[1], matches)};
 
-  cv::Mat imgMatches;
-  cv::drawMatches(images[0], keypoints[0], images[1], keypoints[1], matches, imgMatches);
-  cv::imshow("Matches", imgMatches);
-  cv::waitKey(0);
+  // Testing constructEqnMatrix function
+  std::cout << matchPoints.size() << "\n";
 
-  //for (cv::Mat img : images) {
-  //  cv::imshow("Displaying image", img);
-  //  int key = cv::waitKey(0);
-  //  if (key == 's') {
-  //    continue;
-  //  }
-  //}
+  std::vector<MatchPoints> sampleMatchPoints;
+  std::sample(matchPoints.begin(), matchPoints.end(), std::back_inserter(sampleMatchPoints), 7,
+              std::mt19937{std::random_device{}()});
+  for (auto matchPt : sampleMatchPoints) {
+    std::cout << matchPt.point1 << "," << matchPt.point2 << "\n";
+  }
+  auto eqnMat{constructEqnMatrix(sampleMatchPoints)};
+  std::cout << eqnMat << "\n";
+
+  // Draw matches
+  // cv::Mat imgMatches;
+  // cv::drawMatches(images[0], keypoints[0], images[1], keypoints[1], matches, imgMatches);
+  // cv::imshow("Matches", imgMatches);
+  // cv::waitKey(0);
+
+  // Compute fundamental matrix
+
+  // for (cv::Mat img : images) {
+  //   cv::imshow("Displaying image", img);
+  //   int key = cv::waitKey(0);
+  //   if (key == 's') {
+  //     continue;
+  //   }
+  // }
 
   return 0;
 }
